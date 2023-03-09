fb16bb894751704c9cad8c35bbfc14e0aacbd219
DavidS1920P <davidsoto301129@correo.itm.edu.co>
2023-03-08 19:41:22 -0500

    Add Display In Entity Class

--- E-Commerce-App--Monday-Wednesday/E-Commerce-App--Monday-Wednesday/DAL/Entities/Entity.cs
+++ E-Commerce-App--Monday-Wednesday/E-Commerce-App--Monday-Wednesday/DAL/Entities/Entity.cs
@@ -8,9 +8,10 @@ namespace E_Commerce_App__Monday_Wednesday.DAL.Entities
         [Key]
         [Required]
         public Guid Id { get; set; }
+        [Display(Name = "Creation Date")]
 
         public DateTime CreatedDate { get; set; }
-
+        [Display(Name = "Creation Date")]
         public DateTime ModifiedDate { get; set; }
     }
 }
